7c23149a48dbd475885c5094d07ee23918bb13fa
PTD-LAPTOP <peda_95@hotmail.com>
2019-08-28 11:37:56 +0200

    Added serial support for Arduino

--- java/src/main/java/communication/ArduinoHandlerTest.java
+++ java/src/main/java/communication/ArduinoHandlerTest.java
@@ -1,10 +1,36 @@
 package main.java.communication;
 
+import java.util.Scanner;
+
 public class ArduinoHandlerTest {
 
     public static void main(String[] args) {
         ArduinoHandler arduinoHandler = new ArduinoHandler();
+        arduinoHandler.run();
 
-        // TODO: Create an example of class usage
+        System.out.println("Started");
+
+
+        boolean quit= false;
+        while(!quit){
+            System.out.println("enter a number");
+            Scanner reader = new Scanner(System.in);
+            int menuSelection = reader.nextInt();
+            String hei = "halla";
+            switch (menuSelection){
+                case 1:
+                    arduinoHandler.sendData(hei.getBytes());
+                    break;
+                case 2:
+                    arduinoHandler.sendData(hei.getBytes());
+                    break;
+
+                case 3:
+                    System.out.println("quit");
+                    arduinoHandler.close();
+                    quit=true;
+                    break;
+            }
+        }
     }
 }
